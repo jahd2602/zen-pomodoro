78c1a02bfa60ef4f84c4385b1b6d4c6f29158e6e
Jairo Honorio <jairoh@jairoh-macbookpro.roam.corp.google.com>
2020-04-19 18:48:31 -0300

    Refactor out AppDrawer from App component

--- src/AppComponent/App.js
+++ src/AppComponent/App.js
@@ -11,37 +11,12 @@ import emptyFunction from 'fbjs/lib/emptyFunction';
 import s from './App.scss';
 import TomatoComponent from '../TomatoComponent/Tomato';
 import {
-    Grid,
-    Cell,
     Layout,
-    Navigation,
     Header,
     Content,
-    Icon,
-    Drawer,
-    Button,
-    RadioGroup,
-    Radio,
 } from 'react-mdl';
-import { version } from './../../package.json';
-import {
-    FacebookShareButton,
-    LinkedinShareButton,
-    TwitterShareButton,
-    TelegramShareButton,
-    WhatsappShareButton,
-    PinterestShareButton,
-    RedditShareButton,
-    EmailShareButton,
-    FacebookIcon,
-    TwitterIcon,
-    TelegramIcon,
-    WhatsappIcon,
-    LinkedinIcon,
-    PinterestIcon,
-    RedditIcon,
-    EmailIcon
-} from 'react-share';
+import { AppDrawer } from '../AppDrawerComponent/AppDrawer';
+
 
 class App extends Component {
 
@@ -100,149 +75,7 @@ class App extends Component {
             <div>
                 <Layout fixedDrawer>
                     <Header transparent />
-                    <Drawer className="drawer">
-                        <Grid
-                            shadow={1}
-                            style={{ margin: '0' }}
-                        >
-                            <Cell col={12}>
-                                <h1>Zen Pomodoro</h1>
-                            </Cell>
-                            <Cell col={8}>
-                                <div>
-                                    by Jairo Honorio
-                                </div>
-                                <div className="mdl-color-text--grey">
-                                    version {version}
-                                </div>
-                            </Cell>
-                            <Cell col={4}>
-                                <Button
-                                    ripple
-                                    onClick={this.handleToggleDialog}
-                                >
-                                    <Icon name="settings" />
-                                </Button>
-                            </Cell>
-                            {this.state.openDialog ?
-                                <Cell col={12}>
-                                    <Grid
-                                        shadow={1}
-                                        style={{ margin: '20px 0' }}
-                                    >
-                                        <Cell col={6}>
-                                            Tick sound
-                                        </Cell>
-                                        <Cell col={6}>
-                                            <RadioGroup
-                                                name="tickSoundConfig"
-                                                value={this.state.tickSoundConfig}
-                                                childContainer="div"
-                                                onChange={this.onTickSoundConfigChange}
-                                            >
-                                                <Radio
-                                                    value="2"
-                                                    ripple
-                                                >
-                                                    Starting
-                                                </Radio>
-                                                <Radio
-                                                    value="1"
-                                                    ripple
-                                                >
-                                                    On
-                                                </Radio>
-                                                <Radio
-                                                    value="0"
-                                                    ripple
-                                                >
-                                                    Off
-                                                </Radio>
-                                            </RadioGroup>
-                                        </Cell>
-                                    </Grid>
-                                </Cell>
-                                : null}
-                        </Grid>
-
-                        <Navigation>
-                            <a
-                                href="https://github.com/jahd2602/zen-pomodoro"
-                                target="blank"
-                            >
-                                <Icon name="code" /> Code on Github
-                            </a>
-                            <a
-                                href="https://pe.linkedin.com/in/jairohonorio"
-                                target="blank"
-                            >
-                                <Icon name="portrait" /> My LinkedIn
-                            </a>
-                            <a
-                                href="mailto:jairo@jahdsoft.com"
-                                target="blank"
-                            >
-                                <Icon name="email" /> Email me
-                            </a>
-                        </Navigation>
-
-                        <Grid
-                            shadow={1}
-                            className="socialContainer"
-                        >
-                            <h2>Social Share</h2>
-                            <Cell col={6}>
-                                <FacebookShareButton url={shareUrl}>
-                                    <FacebookIcon round />
-                                </FacebookShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <TwitterShareButton url={shareUrl}>
-                                    <TwitterIcon round />
-                                </TwitterShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <LinkedinShareButton url={shareUrl}>
-                                    <LinkedinIcon round />
-                                </LinkedinShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <WhatsappShareButton url={shareUrl}>
-                                    <WhatsappIcon round />
-                                </WhatsappShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <TelegramShareButton url={shareUrl}>
-                                    <TelegramIcon round />
-                                </TelegramShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <RedditShareButton url={shareUrl}>
-                                    <RedditIcon round />
-                                </RedditShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <PinterestShareButton
-                                    url={shareUrl}
-                                    media="http://zen.jahdsoft.com/images/screenshot.png"
-                                >
-                                    <PinterestIcon round />
-                                </PinterestShareButton>
-                            </Cell>
-
-                            <Cell col={6}>
-                                <EmailShareButton url={shareUrl}>
-                                    <EmailIcon round />
-                                </EmailShareButton>
-                            </Cell>
-                        </Grid>
-                    </Drawer>
+                    <AppDrawer handleToggleDialog={this.handleToggleDialog} openDialog={this.state.openDialog} tickSoundConfig={this.state.tickSoundConfig} onTickSoundConfigChange={this.onTickSoundConfigChange} shareUrl={shareUrl}></AppDrawer>
                     <Content>
                         <TomatoComponent getConfig={this.onGetConfig} />
                     </Content>
---
+++ src/AppDrawerComponent/AppDrawer.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Grid, Cell, Navigation, Icon, Drawer, Button, RadioGroup, Radio } from 'react-mdl';
+import { version } from '../../package.json';
+import { FacebookShareButton, LinkedinShareButton, TwitterShareButton, TelegramShareButton, WhatsappShareButton, PinterestShareButton, RedditShareButton, EmailShareButton, FacebookIcon, TwitterIcon, TelegramIcon, WhatsappIcon, LinkedinIcon, PinterestIcon, RedditIcon, EmailIcon } from 'react-share';
+import s from './AppDrawer.scss';
+
+export class AppDrawer extends React.Component {
+    render() {
+        return (<Drawer className="drawer">
+            <Grid shadow={1} style={{
+                margin: '0'
+            }}>
+                <Cell col={12}>
+                    <h1>Zen Pomodoro</h1>
+                </Cell>
+                <Cell col={8}>
+                    <div>
+                        by Jairo Honorio
+                                </div>
+                    <div className="mdl-color-text--grey">
+                        version {version}
+                    </div>
+                </Cell>
+                <Cell col={4}>
+                    <Button ripple onClick={this.props.handleToggleDialog}>
+                        <Icon name="settings" />
+                    </Button>
+                </Cell>
+                {this.props.openDialog ? <Cell col={12}>
+                    <Grid shadow={1} style={{
+                        margin: '20px 0'
+                    }}>
+                        <Cell col={6}>
+                            Tick sound
+                                        </Cell>
+                        <Cell col={6}>
+                            <RadioGroup name="tickSoundConfig" value={this.props.tickSoundConfig} childContainer="div" onChange={this.props.onTickSoundConfigChange}>
+                                <Radio value="2" ripple>
+                                    Starting
+                                                </Radio>
+                                <Radio value="1" ripple>
+                                    On
+                                                </Radio>
+                                <Radio value="0" ripple>
+                                    Off
+                                                </Radio>
+                            </RadioGroup>
+                        </Cell>
+                    </Grid>
+                </Cell> : null}
+            </Grid>
+
+            <Navigation>
+                <a href="https://github.com/jahd2602/zen-pomodoro" target="blank">
+                    <Icon name="code" /> Code on Github
+                            </a>
+                <a href="https://pe.linkedin.com/in/jairohonorio" target="blank">
+                    <Icon name="portrait" /> My LinkedIn
+                            </a>
+                <a href="mailto:jairo@jahdsoft.com" target="blank">
+                    <Icon name="email" /> Email me
+                            </a>
+            </Navigation>
+
+            <Grid shadow={1} className="socialContainer">
+                <Cell col={12}>
+                    <h5>Share via:</h5>
+                </Cell>
+                <Cell col={6}>
+                    <FacebookShareButton url={this.props.shareUrl}>
+                        <FacebookIcon round />
+                    </FacebookShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <TwitterShareButton url={this.props.shareUrl}>
+                        <TwitterIcon round />
+                    </TwitterShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <LinkedinShareButton url={this.props.shareUrl}>
+                        <LinkedinIcon round />
+                    </LinkedinShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <WhatsappShareButton url={this.props.shareUrl}>
+                        <WhatsappIcon round />
+                    </WhatsappShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <TelegramShareButton url={this.props.shareUrl}>
+                        <TelegramIcon round />
+                    </TelegramShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <RedditShareButton url={this.props.shareUrl}>
+                        <RedditIcon round />
+                    </RedditShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <PinterestShareButton url={this.props.shareUrl} media="http://zen.jahdsoft.com/images/screenshot.png">
+                        <PinterestIcon round />
+                    </PinterestShareButton>
+                </Cell>
+
+                <Cell col={6}>
+                    <EmailShareButton url={this.props.shareUrl}>
+                        <EmailIcon round />
+                    </EmailShareButton>
+                </Cell>
+            </Grid>
+        </Drawer>);
+    }
+}
